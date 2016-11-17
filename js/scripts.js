3485babe4b339e3a0d6091919608aae46878b6e1
Pawel <pawel@nurzynski.com>
2016-11-17 02:29:05 +0100

    Zadanie2

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+
+(function($){
+
+	$(document).ready(function(){
+
+		var $a = $('div.grid.grid-12');
+		var $b = $('.nav a[href^="http"]');
+		var $c = $('input:not(:checked([type=checkbox],[type=radio]))');
+		var $d = $('div#text p:empty:first');
+		var $e = $('.pagination-item:not(span)');
+		console.log($e);
+	});
+
+})(jQuery);
